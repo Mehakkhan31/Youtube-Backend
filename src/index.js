c0373cf721c561dcfb65a14252f07f57cc195bd6
Mehakkhan31 <mehakkhan997740@gmail.com>
2026-01-27 22:06:21 +0530

    update code

--- src/index.js
+++ src/index.js
@@ -13,11 +13,3 @@ connectDB()
 
 
 
-// ( async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//  } catch (error){
-//         console.error("ERROR:", error)
-//         throw err
-//     }
-// })()
