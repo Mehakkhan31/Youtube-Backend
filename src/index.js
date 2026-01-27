8b04d9c1284e1205d2686e025d941a2aec69f71f
Mehakkhan31 <mehakkhan997740@gmail.com>
2026-01-27 22:03:30 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import dotenv  from "dotenv";
+import connectDB from "./db/db.index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
 
 
 
 
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
- } catch (error){
-        console.error("ERROR:", error)
-        throw err
-    }
-})()
+
+
+
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//  } catch (error){
+//         console.error("ERROR:", error)
+//         throw err
+//     }
+// })()
